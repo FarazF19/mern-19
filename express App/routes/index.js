b3fa36cf9cd5eac39051c6ced22be8d625e1ac39
FarazF19 <mfaraz.bees19@iba-suk.edu.pk>
2024-08-02 13:00:04 +0500

    added get apis

--- express App/routes/index.js
+++ express App/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+var router = require("express").Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("Hello from server side");
-});
+
+router.get("/home",(req,res)=>{
+  return res.send("Hello from Home!");
+})
 
 module.exports = router;
--- express App/routes/users.js
+++ express App/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.get('/create',(req,res)=>{
+  return res.send("User created!")
+})
+
+
+router.get('/update',(req,res)=>{
+  return res.send("User Updated!")
+})
+
+router.get('/delete',(req,res)=>{
+  return res.send("User deleted!")
+})
 
 module.exports = router;
