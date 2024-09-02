ef7f6f1fd285e12d8ed179213d9a96eb8e9f58b4
FarazF19 <mfaraz.bees19@iba-suk.edu.pk>
2024-09-02 13:02:59 +0500

    added props, usestate and learnt controlled components

--- React App/vite-project/src/App.jsx
+++ React App/vite-project/src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [items, setItems] = useState(["Jeans", "Shirts"]);
+
+  const inputChangeHandler = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const addItemHandler = () => {
+    setItems([...items, userInput]);
+  };
   return (
     <>
-      <Counter />
-      <Counter />
+      <input
+        value={userInput}
+        onChange={inputChangeHandler}
+        placeholder="Enter an item"
+      />
+      <button onClick={addItemHandler}>Add Item</button>
+      {items.map((item, index, arr) => {
+        return <Counter itemName={item} />;
+      })}
     </>
   );
 }
--- React App/vite-project/src/components/Counter.jsx
+++ React App/vite-project/src/components/Counter.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-function Counter() {
+function Counter(props) {
   let [count, setCount] = useState(0);
   const handleCount = () => {
     setCount(count + 1);
   };
   return (
     <>
+      <h1>{props.itemName}</h1>
       <h1>Count:{count}</h1>
       <button onClick={handleCount}>Increment</button>
       <button
